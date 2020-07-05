3121746ec1d2c0e3fa1ec86d69ada0d256a01dff
Rafael Souto <soutorafez@gmail.com>
2020-07-05 20:04:44 -0300

    fix in import

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-import kernel from './app/Http/Kernel'
+import kernel from './app/http/Kernel'
 
 const port = process.env.APP_PORT || 3333;
 const message = () => console.log(`Server running on ${port}`);
